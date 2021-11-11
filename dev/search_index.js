fde870192e8e23fc55238a809274a72b666e4321
Documenter.jl <documenter@juliadocs.github.io>
2021-11-11 04:03:43 +0000

    build based on 97a69be

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMLMDriftCorrection","category":"page"},{"location":"#SMLMDriftCorrection","page":"Home","title":"SMLMDriftCorrection","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SMLMDriftCorrection.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMLMDriftCorrection]","category":"page"}]
+}
