9605ffcb07e1415f97d07b6d8cf5a37ff3c2c1f3
Michael Wester <wester@salud.unm.edu>
2023-02-13 12:44:06 -0700

    Plots fixed in example; runtests RMSD test added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,34 @@
 using SMLMDriftCorrection
+DC = SMLMDriftCorrection
+using SMLMSim
 using Test
 
 @testset "SMLMDriftCorrection.jl" begin
     # Write your tests here.
+
+    # make an Nmer dataset
+    γ=1e5 # Fluorophore emission rate
+    q=[0 50
+       1e-2 0] # Fluorophore blinking rates
+    n=6 # Nmer rank
+    d=.1 # Nmer diameter
+    ρ=0.1 # density of Nmers
+    xsize=25.6 # image size
+    ysize=25.6
+    nframes=2000 # number of frames
+    framerate=50.0 # framerate
+    σ_psf=1.3 # psf sigma used for uncertainty calcs
+    minphotons=500 # minimum number of photons per frame accepted
+    # Simulation sequence
+    f=SMLMSim.GenericFluor(γ,q)
+    pattern=SMLMSim.Nmer2D(; n,d)
+    smd_true=SMLMSim.uniform2D(ρ,pattern,xsize,ysize)
+    smd_model=SMLMSim.kineticmodel(smd_true,f,nframes,framerate;ndatasets=10,minphotons=minphotons)
+    smd_noisy=SMLMSim.noise(smd_model,σ_psf)
+    ## Set up drift model
+    driftmodel=DC.Polynomial(smd_noisy; degree=2, initialize="random")
+    smd_drift=DC.applydrift(smd_noisy,driftmodel)
+    smd_DC=DC.correctdrift(smd_drift,driftmodel)
+    rmsd = sqrt(sum((smd_DC.x .- smd_noisy.x).^2 .+ (smd_DC.y .- smd_noisy.y).^2))
+    @test isapprox(rmsd, 0.0; atol=1e-10)
 end
