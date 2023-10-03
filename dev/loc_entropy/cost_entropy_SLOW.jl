7bd6ea7dd0328eecd302932768966904f796c067
Michael Wester <wester@salud.unm.edu>
2023-10-03 13:20:02 -0600

    Speed ups to cost entropy.

--- dev/loc_entropy/cost_entropy_SLOW.jl
+++ dev/loc_entropy/cost_entropy_SLOW.jl
@@ -29,20 +29,12 @@ t_1, t_2, respectively.
 """
 function divKL(x1::Vector{T}, s1::Vector{T},
     x2::Vector{T}, s2::Vector{T}) where {T<:Real}
-    # K is the dimension of the space, typically 2.
-    K = length(x1)
-    
+
     si2 = s1 .^ 2
     sj2 = s2 .^ 2
-
-    # out = 1 / 2 * sum(log.(s2 .^ 2 ./ s1 .^ 2) + s1 .^ 2 ./ s2 .^ 2 + (x1 - x2) .^ 2 ./ s2 .^ 2) - K / 2
-    out = T(0)
-    for i in 1:K
-        out += log(sj2[i] / si2[i]) + si2[i] / sj2[i] + (x1[i] - x2[i]) ^ 2 / sj2[i]
-    end
-    out -= T(K)
-    out /= T(2)
- 
+    # K is the dimension of the space, typically 2.
+    K = length(x1)
+    out = 1 / 2 * sum(log.(sj2 ./ si2) + si2 ./ sj2 + (x1 - x2) .^ 2 ./ sj2) - K / 2
     return out
 end
 
@@ -62,35 +54,26 @@ function entropy1(idxs::Vector{Vector{Int}}, x::Vector{T}, y::Vector{T},
     # maxn is the maxinum number of neighbors allowed.
     maxn = length(idxs[1]) - 1
 
-    kldiv = zeros(T, maxn)
+    kldiv = zeros(Float32, maxn)
     #Threads.@threads 
     # length(x) number of localizations
     # maxn      number of neighbors per localizations
     # NOTE: kldiv is reused for each i
-    # println("length(x) = ", length(x), ", maxn = ", maxn)
-    # print("In entropy1: ")
-    # @time begin
-    r1 = Vector{T}(undef, 2)
-    σ1 = Vector{T}(undef, 2)
-    r2 = Vector{T}(undef, 2)
-    σ2 = Vector{T}(undef, 2)
-
     for i = 1:length(x)
-        idx = idxs[i]
-        r1 .= [x[i], y[i]]
-        σ1 .= [σ_x[i], σ_y[i]]
-        
         for j = 2:maxn + 1
-            r2 .= [x[idx[j]], y[idx[j]]]
-            σ2 .= [σ_x[idx[j]], σ_y[idx[j]]]
-            kldiv[j-1] = divKL(r1, σ1, r2, σ2)    
+            idx = idxs[i]
+            r1 = [x[i], y[i]]
+            r2 = [x[idx[j]], y[idx[j]]]
+            σ1 = [σ_x[i], σ_y[i]]
+            σ2 = [σ_x[idx[j]], σ_y[idx[j]]]
+            kldiv[j-1] = divKL(r1, σ1, r2, σ2)
         end
-    
-        out += logsumexp(- kldiv) - log(T(length(kldiv)))
+
+        out += logsumexp(- kldiv) - log(length(kldiv))
+
     end
 
-    # end # @time
-
+    
     N = length(x)   # total number of localizations
     return entropy_HD(σ_x, σ_y) - out / N
 end
@@ -99,9 +82,9 @@ end
 Entropy upper bound based on maxn nearest neighbors of each localization.
 
 # Fields
-- x, y       vector of localization positions
-- σ_x, σ_y   vector of localization uncertainties
-- maxn:      maximum number of neighbors considered
+- x, y:       vector of localization positions
+- σ_x, σ_y:   vector of localization uncertainties
+- maxn:       maximum number of neighbors considered
 """
 function ub_entropy(x::Vector{T}, y::Vector{T},
     σ_x::Vector{T}, σ_y::Vector{T}; maxn::Int = 200
@@ -112,14 +95,12 @@ function ub_entropy(x::Vector{T}, y::Vector{T},
 
     data = transpose(coords)
 
-    #print("KDTree: ") @time 
+    #maxn = 200
     kdtree = KDTree(data; leafsize=10)
     # true below so that results are sorted into increasing order of distance
-    #print("knn: ") @time
     idxs, dists = knn(kdtree, data, maxn + 1, true)
     #idxs = inrange(kdtree, data, maxn + 1)
     #println("Calculating Entropy...")
-    #@time 
-    entropy1(idxs, x, y, σ_x, σ_y)
+    return entropy1(idxs, x, y, σ_x, σ_y)
 
 end
