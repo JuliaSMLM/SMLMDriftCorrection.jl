93390bc0838e2b5f1875e3e60e2948b4222f3a19
Michael Wester <wester@salud.unm.edu>
2023-11-08 17:02:10 -0700

    Added a routine for a histogram image.

--- dev/crosscorr.jl
+++ dev/crosscorr.jl
@@ -1,22 +1,34 @@
 using Revise
-using FourierTools
+#using FourierTools
 
 """
 Produce a histogram image from the localization coordinates x and y,
+x and y are assumed to be in nm.
+histbinsize is the size of the bins in nm.
 """
-function histimage(x, y; histbinsize=5)
-    # Compute histogram
-    h = fit(Histogram, x, nbins)
-    # Compute image
-    img = zeros(nbins, nbins)
-    for i in 1:nbins
-        for j in 1:nbins
-            img[i, j] = h.weights[i, j]
-        end
+function histimage(x::Vector{T}, y::Vector{T}; histbinsize::T=5.0
+    ) where {T<:Real}
+    x_min = minimum(x)
+    x_max = maximum(x)
+    y_min = minimum(y)
+    y_max = maximum(y)
+    # Compute the number of pixels in x and y.
+    imszX = round(Int, (x_max .- x_min) ./ histbinsize)
+    imszY = round(Int, (y_max .- y_min) ./ histbinsize)
+    # Create a blank image.
+    im = zeros(imszX, imszY)
+    # Convert (x, y) coordinates into pixel units.
+    xx = round.(Int, (x .- x_min) ./ histbinsize)
+    yy = round.(Int, (y .- y_min) ./ histbinsize)
+    # Exclude points that are outside the image dimensions.
+    mask = (xx .> 0) .& (xx .<= imszX) .& (yy .> 0) .& (yy .<= imszY)
+    xx = xx[mask]
+    yy = yy[mask]
+    # Make the histogram image.
+    for i in 1:size(xx, 1)
+        im[xx[i], yy[i]] += 1
     end
-    # Compute x and y bins
-    xbins = range(minimum(x), stop=maximum(x), length=nbins+1)
-    ybins = range(minimum(y), stop=maximum(y), length=nbins+1)
-    # Return image and bins
-    return img, xbins, ybins
+
+    # Return image
+    return im
 end
