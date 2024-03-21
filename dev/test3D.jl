05e2baece62571e02f6fca4c28c5c6c6e9b96304
Michael Wester <wester@salud.unm.edu>
2024-03-21 15:22:49 -0600

    Add an example for testing 3D

---
+++ dev/test3D.jl
@@ -0,0 +1,36 @@
+using Revise
+using JLD2
+using FileIO
+using SMLMData
+using SMLMDriftCorrection
+
+function convert2D(smld3::SMLMData.SMLD3D)
+    smld2 = SMLMData.SMLD2D(smld3.ndatasets)
+    smld2.connectID = smld3.connectID
+    smld2.x = smld3.x
+    smld2.y = smld3.y
+    smld2.σ_x = smld3.σ_x
+    smld2.σ_y = smld3.σ_y
+    smld2.photons = smld3.photons
+    smld2.σ_photons = smld3.σ_photons
+    smld2.bg = smld3.bg
+    smld2.σ_bg = smld3.σ_bg
+    smld2.framenum = smld3.framenum
+    smld2.datasetnum = smld3.datasetnum
+    #smld2.datasize = smld3.datasize
+    #smld2.nframes = smld3.nframes
+    #smld2.ndatasets = smld3.ndatasets
+    return smld2
+end
+
+dir = "Y:\\Projects\\Super Critical Angle Localization Microscopy\\Data\\10-06-2023\\Data2\\old insitu psf and stg pos"
+file = "Data2-2023-10-6-17-11-54deepfit1.jld2"
+filepath = joinpath(dir, file)
+# Load the file
+data = load(filepath) #To check keys use, varnames = keys(data)
+# Get smld
+smld3 = data["smld"]
+
+smld2 = convert2D(smld3)
+
+smld_DC = SMLMDriftCorrection.driftcorrect(smld2; cost_fun = "Kdtree")
